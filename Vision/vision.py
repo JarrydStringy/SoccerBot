c3ac41f38b15538a3a09f8ae98b10997f3e26cba
Jarryd Stringfellow <jarryd.stringfellow@gmail.com>
2019-09-12 18:39:12 +1000

    Working colour detection with border

--- Vision/vision.py
+++ Vision/vision.py
@@ -1,29 +1,83 @@
-# Imports
+# import modules
 import cv2
 import numpy as np
+# import picamera
+# from picamera.array import PiArrayOutput
+# from picamera import PiCamera
 
+# Exposure off
+# camera = PiCamera(sensor_mode=2)
+# camera.exposure_mode = 'off'      # ... locks gains
+
+# Auto white balance set value
+# camera.awb_mode = 'off'
+
+# Begin video capture
 cap = cv2.VideoCapture(0)  # Connect to camera 0 (or the only camera)
 cap.set(3, 320)  # Set the width to 320
 cap.set(4, 240)  # Set the height to 240
-ret, frame = cap.read()  # Get a frame from the camera
 
-if ret:  # Check if data was obtained successfully
-    cv2.imshow("CameraImage", frame)  # Display the obtained frame in a window called "CameraImage"
-    cv2.waitKey(0)  # Make the program wait until you press a key before continuing.
+while 1:
+    # Check each individual frame
+    ret, frame = cap.read()
+    # blur frame
+    blur = cv2.GaussianBlur(frame, (5, 5), 0)
+    # apply canny edge detection to image
+    # Canny(image, threshold1, threshold2
+    edge = cv2.Canny(blur, 100, 200)
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Convert from BGR to HSV colour space)
-    cv2.imshow("HSV_IMAGE", hsv_frame)
+    # define ranges of spectrum in HSV[] for each object
+    # Ball=============================================
+    lower_ball = np.array([0, 122, 91])
+    upper_ball = np.array([255, 250, 255])
 
-    h, s, v = cv2.split(frame)
-    h *= 0
-    v *= 0 
+    # Blue goal========================================
+    lower_blue = np.array([115, 59, 34])
+    upper_blue = np.array([152, 161, 136])
 
-    hsv_frame = cv2.merge((h, s, v))
-    cv2.imshow("HSV_IMAGE", hsv_frame)
+    # Yellow goal======================================
+    lower_yellow = np.array([17, 178, 73])
+    upper_yellow = np.array([30, 255, 193])
 
-    
-    cv2.waitKey(0)  # Exit on keypress
+    # Obstacles========================================
+    lower_obstacle = np.array([249, 53, 0])
+    upper_obstacle = np.array([50, 160, 82])
+
+    # Threshold the HSV image
+    ball_mask = cv2.inRange(hsv, lower_ball, upper_ball)
+    blue_mask = cv2.inRange(hsv, lower_ball, upper_ball)
+    yellow_mask = cv2.inRange(hsv, lower_ball, upper_ball)
+    obstacle_mask = cv2.inRange(hsv, lower_ball, upper_ball)
+
+    # Bitwise-AND mask and original image
+    newFrame = cv2.bitwise_and(frame, frame, mask=ball_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=blue_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=yellow_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=obstacle_mask)
+
+    # Display steps of masking images as video
+    cv2.imshow('frame', frame)
+    # cv2.imshow('blur', blur)
+    cv2.imshow('edge', edge)
+    # cv2.imshow('mask', ball_mask)
+    cv2.imshow('new', newFrame)
+
+    # ***********************************************************************
+
+
+
+    # ***********************************************************************
+
+    # & 0xFF sets left bits to 0 for keyboard input whilst waiting for input
+    k = cv2.waitKey(5) & 0xFF
+    # k == 27 is for Esc key
+    if k == 27:
+        break
+
+    # cv2.waitKey(0)  # Exit on keypress
 
 cap.release()  # Release the camera object
-cv2.destroyAllWindows()  # Close all opencv pop-up windows
-
+# stops program
+cv2.destroyAllWindows()
--- Vision/visionTest.py
+++ Vision/visionTest.py
@@ -1,35 +1,50 @@
+# import modules
 import cv2
 import numpy as np
+# import picamera
+# from picamera.array import PiArrayOutput
+# from picamera import PiCamera
 
-cap = cv2.VideoCapture(0)
+# Exposure off
+# camera = PiCamera(sensor_mode=2)
+# camera.exposure_mode = 'off'      # ... locks gains
 
-while(1):
-    # Take each frame
-    _, frame = cap.read()
+# Auto white balance set value
+# camera.awb_mode = 'off'
 
+# Begin video capture
+cap = cv2.VideoCapture(0)  # Connect to camera 0 (or the only camera)
+cap.set(3, 320)  # Set the width to 320
+cap.set(4, 240)  # Set the height to 240
+
+while 1:
+    # Check each individual frame
+    ret, frame = cap.read()
+    # blur frame
+    blur = cv2.GaussianBlur(frame, (5, 5), 0)
+    # apply canny edge detection to image
+    # Canny(image, threshold1, threshold2
+    edge = cv2.Canny(blur, 100, 200)
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #Ball=============================================
-    # define range in HSV
-    #lower = np.array([250,122,91])
-    #upper = np.array([251,250,255])
+    # define ranges of spectrum in HSV[] for each object
+    # Ball=============================================
     lower_ball = np.array([0, 122, 91])
-    upper_ball = np.array([255, 250, 255])     
+    upper_ball = np.array([255, 250, 255])
 
-    #Blue goal========================================
+    # Blue goal========================================
     lower_blue = np.array([115, 59, 34])
     upper_blue = np.array([152, 161, 136])
 
-    #Yellow goal======================================
+    # Yellow goal======================================
     lower_yellow = np.array([17, 178, 73])
     upper_yellow = np.array([30, 255, 193])
 
-    #Obstacles========================================
+    # Obstacles========================================
     lower_obstacle = np.array([249, 53, 0])
     upper_obstacle = np.array([50, 160, 82])
-    
-    
+
     # Threshold the HSV image
     ball_mask = cv2.inRange(hsv, lower_ball, upper_ball)
     blue_mask = cv2.inRange(hsv, lower_ball, upper_ball)
@@ -37,16 +52,32 @@ while(1):
     obstacle_mask = cv2.inRange(hsv, lower_ball, upper_ball)
 
     # Bitwise-AND mask and original image
-    new = cv2.bitwise_and(frame, frame, mask= ball_mask)
-    new = cv2.bitwise_and(frame, frame, mask= blue_mask)
-    new = cv2.bitwise_and(frame, frame, mask= yellow_mask)
-    new = cv2.bitwise_and(frame, frame, mask= obstacle_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=ball_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=blue_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=yellow_mask)
+    newFrame = cv2.bitwise_and(frame, frame, mask=obstacle_mask)
 
-    cv2.imshow('frame',frame)
-    #cv2.imshow('mask',ball_mask)
-    cv2.imshow('new',new)
+    # Display steps of masking images as video
+    cv2.imshow('frame', frame)
+    # cv2.imshow('blur', blur)
+    cv2.imshow('edge', edge)
+    # cv2.imshow('mask', ball_mask)
+    cv2.imshow('new', newFrame)
+
+    # ***********************************************************************
+
+
+
+    # ***********************************************************************
+
+    # & 0xFF sets left bits to 0 for keyboard input whilst waiting for input
     k = cv2.waitKey(5) & 0xFF
+    # k == 27 is for Esc key
     if k == 27:
         break
 
+    # cv2.waitKey(0)  # Exit on keypress
+
+cap.release()  # Release the camera object
+# stops program
 cv2.destroyAllWindows()
