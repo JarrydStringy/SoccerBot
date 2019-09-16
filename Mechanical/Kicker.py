90e7ea5a47967c3a2124d2ef0fe2e4d21056e02a
Jarryd Stringfellow <jarryd.stringfellow@gmail.com>
2019-09-16 11:06:15 +1000

    Updated Kicker

--- Mechanical/Kicker.py
+++ Mechanical/Kicker.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)			# Set the GPIO pin naming convention to BCM
 GPIO.setup(21,GPIO.OUT)			# Set up GPIO pin 21 as an output
 
 while True:
-  print('Test')
+  print('Kick')
   GPIO.output(21,GPIO.HIGH) 	# Set GPIO pin 21 to digital high (on)
   time.sleep(2)	    			# Wait
   GPIO.output(21,GPIO.LOW)		# Set GPIO pin 21 to digital low (off)
--- Vision/ballValues.py
+++ Vision/ballValues.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 import imutils
+import time
 
 # define ranges of spectrum in HSV[] for each object
 # Ball=============================================
@@ -20,10 +21,17 @@ cap = cv2.VideoCapture(0)  # Connect to camera 0 (or the only camera)
 cap.set(3, 320)  # Set the width to 320
 cap.set(4, 240)  # Set the height to 240
 
+# ================================================================
+
+# =============================================
+
 
 def DistanceToCamera(knownWidth, focalLength, perWidth):
     # compute and return the distance from the maker to the camera
-    return (knownWidth * focalLength) / perWidth
+    if perWidth != 0:
+        return (knownWidth * focalLength) / perWidth
+    else:
+        return 0
 
 
 def DistanceB(image):
@@ -42,7 +50,7 @@ def DistanceB(image):
         box = np.int0(box)
         cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
         cv2.putText(frame, "b: %.2fm" % dist_ball,
-                    (frame.shape[1] - 100, frame.shape[0] - 70), cv2.FONT_HERSHEY_SIMPLEX,
+                    (frame.shape[1] - 100, frame.shape[0] - 100), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
 
 
@@ -62,7 +70,7 @@ def DistanceO(image):
         box = np.int0(box)
         cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
         cv2.putText(frame, "o: %.2fm" % dist_obstacle,
-                    (frame.shape[1] - 100, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    (frame.shape[1] - 100, frame.shape[0] - 40), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
 
 
@@ -86,7 +94,7 @@ def BearingB(image):
         box = np.int0(box)
 
         cv2.putText(frame, "b: %.2fdeg" % angle_obstacle,
-                    (frame.shape[1] - 100, frame.shape[0] - 100), cv2.FONT_HERSHEY_SIMPLEX,
+                    (frame.shape[1] - 100, frame.shape[0] - 70), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
 
 
@@ -99,19 +107,22 @@ def BearingO(image):
 
         marker = cv2.minAreaRect(c)
         angle_ball = BearingToCamera(marker[1][0], DEG_PER_PX)
-        print("Angle to Object: " + str(angle_ball))
+        print("Angle to Obstacle: " + str(angle_ball))
 
         box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
         box = np.int0(box)
 
         cv2.putText(frame, "o: %.2fdeg" % angle_ball,
-                    (frame.shape[1] - 100, frame.shape[0] - 40), cv2.FONT_HERSHEY_SIMPLEX,
+                    (frame.shape[1] - 100, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
 
 
 while True:
+    startTime = time.time()
     # capture each frame
     ret, frame = cap.read()
+    # Flip image
+    frame = cv2.flip(frame, 0)
     # blur frames with gaussian 5x5 kernel and determined std dev
     blur = cv2.GaussianBlur(frame, (5, 5), 0)
     # Convert BGR to HSV
@@ -140,6 +151,17 @@ while True:
     BearingO(edged_obstacle)
     # =================================================================
 
+    # Calculate FPS
+    endTime = time.time()
+    timeTot = endTime - startTime
+    # 1 frame / time taken to process
+    if timeTot != 0.0:
+        fps = 1 / timeTot
+    print("FPS: %.2f" % fps)
+    cv2.putText(frame, "FPS: %.2f" % fps,
+                (frame.shape[1] - 320, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+
     # Display steps of masking images as video
     cv2.imshow('frame', frame)
     # cv2.imshow('blur', blur)
@@ -155,6 +177,7 @@ while True:
     if k == 27:
         break
     # cv2.waitKey(0)  # Exit on keypress
+
 
 cap.release()  # Release the camera object
 cv2.destroyAllWindows()  # stops program
